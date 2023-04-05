5af9efc213492cdaaf80deb9dca87cf8d0b931aa
GautamNag <gautamnag279@gmail.com>
2023-04-05 21:53:32 +0530

    explanation added

--- Codeforces/800/1374A - Required Remainder.py
+++ Codeforces/800/1374A - Required Remainder.py
@@ -10,3 +10,12 @@ if __name__ == "__main__":
     for i in range(items):
         x, y, n = list(map(int, input().split()))
         maxPossible(x, y, n)
+
+'''
+Here's an easier way to approach the problem:
+
+- Start with the largest possible value of k, which is n.
+- Subtract the remainder of n divided by x from k. This gives us the largest number less than or equal to n that is divisible by x.
+- Add y to k to get the largest number less than or equal to n that leaves a remainder of y when divided by x.
+- If the result is greater than n, subtract x from k to get the largest number less than or equal to n that leaves a remainder of y when divided by x.
+'''
