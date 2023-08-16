7c7b16012b3eed7b3129edf861d4caa69eeda454
GautamNag <gautamnag279@gmail.com>
2023-08-16 20:17:16 +0530

    removed dsa sheet

---
+++ Codeforces/900/1295A - Display The Number.py
@@ -0,0 +1,17 @@
+def display(n):
+    lookup = {
+        0: 6,
+        1: 2,
+        2: 5,
+        3: 5,
+        4: 4,
+        5: 5,
+        6: 6,
+        7: 3,
+        8: 7,
+        9: 6}
+
+
+if __name__ == "__main__":
+    for i in range(int(input())):
+        display(int(input()))
--- DSA Sheet/README.md
+++
@@ -1,3 +0,0 @@
-The code provided only has the solution's function block written. You'll have to write the 'main' function seperately.
-
-<i>Cheers!
