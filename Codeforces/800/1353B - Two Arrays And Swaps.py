5e2e09efcfe38e4048a9dbabcb0f391d21424acd
GautamNag <gautamnag279@gmail.com>
2023-08-18 19:19:38 +0530

    1353b solution (optimized)

--- Codeforces/800/1353B - Two Arrays And Swaps.py
+++ Codeforces/800/1353B - Two Arrays And Swaps.py
@@ -1,11 +1,9 @@
 def solve(a, b, n, k):
     a.sort()
     b.sort(reverse=True)
-    for i in range(k):
-        for j in range(n):
-            if b[j] > a[j]:
-                a[j], b[j] = b[j], a[j]
-                break
+    for i in range(n):
+        if i < k and b[i] > a[i]:
+            a[i] = b[i]
     total_sum = sum(a)
     print(total_sum)
 
@@ -13,7 +11,7 @@ def solve(a, b, n, k):
 if __name__ == "__main__":
     t = int(input())
     for i in range(t):
-        n, k = map(int, input().split())  # 5,3
-        a = list(map(int, input().split()))  # [1,2,3,4,5]
-        b = list(map(int, input().split()))  # [10,9,10,10,9]
+        n, k = map(int, input().split())
+        a = list(map(int, input().split()))
+        b = list(map(int, input().split())) 
         solve(a, b, n, k)
