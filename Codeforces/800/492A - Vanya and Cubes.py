379272862e4d8c32a0c1d85457598dc984f7f9ce
GautamNag <gautamnag279@gmail.com>
2023-08-21 12:24:12 +0530

    492a solution

---
+++ Codeforces/800/492A - Vanya and Cubes.py
@@ -0,0 +1,14 @@
+def solve(n):
+    i = 0
+    element = 0
+    sum = 0
+    while sum <= n:
+        i += 1
+        element += i
+        if (sum>n):
+            break
+        sum += element
+    print(i-1) 
+
+if __name__ == "__main__":
+    solve(int(input()))
