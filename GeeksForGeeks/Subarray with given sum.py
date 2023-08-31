f0aee1d570d424069302569bfcb5a61dabb3aaa0
GautamNag <gautamnag279@gmail.com>
2023-08-31 18:49:27 +0530

    problem added

---
+++ GeeksForGeeks/Subarray with given sum.py
@@ -0,0 +1,31 @@
+# Approach 1/2
+def subArraySum(arr, n, s):
+    left, right, currSum = 0, 0, 0
+    for i in range(n):
+        currSum += arr[i]
+        if (currSum > s):
+            while currSum > s:
+                currSum -= arr[left]
+                left += 1
+        if (currSum == s):
+            right = i
+            break
+    if (currSum != s):
+        return [-1]
+    else:
+        return left+1, right+1
+
+# Approach 2/2
+def subArraySum(arr, n, s):
+    hashMap = {}
+    currSum = 0
+    for i in range(n):
+        currSum += arr[i]
+        if currSum == s:
+            return [1, i+1]
+        
+        diff = currSum - s
+        if diff in hashMap:
+            return [hashMap[diff], i]
+        hashMap[currSum] = 1
+    return [-1]
