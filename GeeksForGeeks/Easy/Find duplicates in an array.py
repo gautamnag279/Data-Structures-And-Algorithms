bdc241b324bb6cca865f271f2d2abbe5498974c8
GautamNag <gautamnag279@gmail.com>
2023-09-13 15:43:29 +0530

    new problem

---
+++ GeeksForGeeks/Easy/Find duplicates in an array.py
@@ -0,0 +1,12 @@
+def duplicates(arr, n):
+    seen, duplicates = set(), set()
+
+    for i in arr:
+        if i in seen:
+            duplicates.add(i)
+        else:
+            seen.add(i)
+
+    if not duplicates:
+        return -1
+    return sorted(list(duplicates))
