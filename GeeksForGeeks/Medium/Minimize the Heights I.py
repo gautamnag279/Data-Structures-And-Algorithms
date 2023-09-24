1a40b807b1fdb88ab388d15514c0bb93a7dfa299
GautamNag <gautamnag279@gmail.com>
2023-09-24 19:49:59 +0530

    new MEDIUM problem

---
+++ GeeksForGeeks/Medium/Minimize the Heights I.py
@@ -0,0 +1,18 @@
+def getMinDiff(self, arr, n, k):
+    arr.sort()
+    
+    diff = arr[n-1] - arr[0]
+    
+    tallest = arr[n-1] - k
+    shortest = arr[0] + k
+    
+    curr_tallest = 0
+    curr_shortest = 0
+    
+    for i in range(n-1):
+        curr_tallest = max(arr[i]+k, tallest)
+        curr_shortest = min(arr[i+1]-k, shortest)
+        
+        diff = min(diff, curr_tallest - curr_shortest)
+    
+    return diff
