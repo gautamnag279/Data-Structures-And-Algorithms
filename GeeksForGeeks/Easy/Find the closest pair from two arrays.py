c755c392887d8d9b608658f9d9d124314f2eea2f
GautamNag <gautamnag279@gmail.com>
2023-09-27 14:53:36 +0530

    new EASY problem

---
+++ GeeksForGeeks/Easy/Find the closest pair from two arrays.py
@@ -0,0 +1,22 @@
+def printClosest(arr, brr, arrSize, brrSize, target):
+    diff = float('inf')
+    curr_sum = 0
+
+    left_index, right_index = 0, brrSize-1
+    pairs = None
+
+    while left_index < arrSize and right_index >= 0:
+        curr_sum = arr[left_index] + brr[right_index]
+        curr_diff = abs(curr_sum - target)
+
+        if curr_diff < diff:
+            diff = curr_diff
+            pairs = [arr[left_index], brr[right_index]]
+
+        if curr_sum < target:
+            left_index += 1
+
+        else:
+            right_index -= 1
+            
+    return pairs
