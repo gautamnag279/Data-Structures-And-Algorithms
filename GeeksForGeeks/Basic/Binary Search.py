841e6c0665660101b7aa64d5fb7b75de60ec315a
GautamNag <gautamnag279@gmail.com>
2023-09-30 18:29:36 +0530

    indentation fix

--- GeeksForGeeks/Basic/Binary Search.py
+++ GeeksForGeeks/Basic/Binary Search.py
@@ -1,12 +1,12 @@
-class Solution:	
+class Solution:
 	def binarysearch(self, arr, n, k):
 		left, right = 0, n-1
 		while left <= right:
-		    mid = (left+right)//2
-		    if arr[mid] == k:
-		        return mid
-	        elif k > arr[mid]:
-	            left = mid+1
-            else:
-                right = mid-1
-        return -1
+			mid = (left+right)//2
+			if arr[mid] == k:
+				return mid
+			elif k > arr[mid]:
+				left = mid+1
+			else:
+				right = mid-1
+		return -1
