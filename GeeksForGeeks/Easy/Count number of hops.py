44c81739089f22a4ba275cbe53b0161935ad16c3
GautamNag <gautamnag279@gmail.com>
2023-09-30 16:09:02 +0530

    new EASY problem

--- GeeksForGeeks/Easy/Count number of hops.py
+++ GeeksForGeeks/Easy/Count number of hops.py
@@ -1,3 +1,22 @@
+# Approach 1/2 - Intuitive
+def countWays(n):
+    if n == 1:
+        return 1
+    if n == 2:
+        return 2
+    if n == 3:
+        return 4
+
+    a, b, c, ans = 1, 2, 4, 0
+    for i in range(4, n+1):
+        ans = (a+b+c) % 1000000007
+        a = b
+        b = c
+        c = ans
+    return ans
+
+
+# Approach 2/2 - Dynamic Programming
 def countWays(n):
     ways = [0] * (n + 1)
 
