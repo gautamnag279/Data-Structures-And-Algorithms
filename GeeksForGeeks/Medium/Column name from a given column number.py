aa977b7e7b633f4b5d2b2602cf8d4c0c79db8b8c
GautamNag <gautamnag279@gmail.com>
2023-10-03 18:35:58 +0530

    POTD -> medium

--- GeeksForGeeks/Medium/Column name from a given column number.py
+++ GeeksForGeeks/Medium/Column name from a given column number.py
@@ -2,7 +2,7 @@ class Solution:
     def colName (self, n):
         ans = ''
         while n:
-            remainder = (n-1)%26
-            ans = chr(65 + remainder) + ans
+            alphabet_to_add = (n-1)%26
+            ans = chr(65 + alphabet_to_add) + ans
             n = (n-1)//26
         return ans
